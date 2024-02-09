92889db691138df7ce4fc0d7d29a3c13bf96c85a
Ayon Sarkar <ayonsarkar380@gmail.com>
2024-02-10 01:33:24 +0530

    v0.171 -> Added Mete ThemeColor Tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,16 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
+import type { Viewport } from "next";
+import Head from 'next/head';
 import StoreProvider from "@/store/StoreProvider";
 const lucy = localFont({
   src: "/fonts/lucy.ttf",
   variable: "--font-lucy",
 });
+export const viewport: Viewport = {
+  themeColor: 'violet',
+}
 const rethink = localFont({
   src: [
     {
@@ -36,6 +41,7 @@ export const metadata: Metadata = {
   icons: {
     icon: '',
   },
+  // theme-color: "#4285f4",
 };
 
 export default function RootLayout({
@@ -45,6 +51,10 @@ export default function RootLayout({
 }) {
   return (
     <StoreProvider>
+      <Head>
+        {/* Add the meta tag for theme-color */}
+        <meta name="theme-color" content="#4285f4" />
+      </Head>
       <html lang="en" className={`${lucy.variable} ${rethink.variable}`}>
         <body className={inter.className}>{children}</body>
       </html>
